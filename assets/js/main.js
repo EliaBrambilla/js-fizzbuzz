5a6e96a143ac3e5f86ec5c64a4c72c45215c6198
EliaBrambilla <elia.brambilla@icloud.com>
2022-10-21 14:43:47 +0200

    aggiungo calcoli contatore da 1 a 100 e lo associo all'output html

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,33 @@ let finalOutput = null;
 //ELEMENTO
 const rowOfContainer = document.querySelector('.row');
 
+//CONTATORE DA 1 A 100
+for(let i = 1; i <= maxNumber; i++){
 
+ 
+    const cardCreated = document.createElement('div');
+    cardCreated.className = 'card';
+  
+    
+    if(!(i % 3)){
+      if((!(i % 3)) && (!(i % 5))){
+        finalOutput = "FizzBuzz"
+        cardCreated.classList.add('fizzbuzz');
+        console.log(finalOutput);
+      }else{
+        finalOutput = "Fizz"
+        cardCreated.classList.add('fizz');
+        console.log(finalOutput);
+      }
+    }else if(!(i % 5)){
+      finalOutput = "Buzz"
+      cardCreated.classList.add('buzz');
+      console.log(finalOutput);
+    }else{
+      finalOutput = i
+    }
+    
+    cardCreated.innerHTML = finalOutput
+    rowOfContainer.append(cardCreated);
+  }
+  
